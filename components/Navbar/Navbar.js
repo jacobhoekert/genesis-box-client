e40ad1c14bfe56e6be30781fcabe39666af537ac
Jacob Hoekert <jshoekert@gmail.com>
2021-01-14 16:10:49 -0500

    fixed navbar hovers

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 
 export const Navbar = () => {
   const [isDroppedDown, setIsDroppedDown] = useState(false);
+  const [isSearchBlue, setSearchBlue] = useState(false);
+  const [isBagBlue, setBagBlue] = useState(false);
 
   const handleDropDownEnter = () => {
     setIsDroppedDown(true);
@@ -16,10 +18,20 @@ export const Navbar = () => {
     <div id="navbar">
       <div className="navbar-menu-container">
         <div className="navbar-menu">
-          <Link href="/"><p>Home</p></Link>
+          <Link href="/">
+            <div className="menu-item">
+              <p>Home</p>
+            </div>
+          </Link>
           <div className="about-container">
             <div className="about-link" >
-              <Link href="/about"><p onMouseEnter={handleDropDownEnter} onMouseLeave={handleDropDownLeave}>About Us</p></Link>
+              <Link href="/about">
+                <div onMouseEnter={handleDropDownEnter} onMouseLeave={handleDropDownLeave} className="menu-item">
+                  <p >
+                    About Us
+                  </p>
+                </div>
+              </Link>
             </div>
             <div onMouseEnter={handleDropDownEnter} onMouseLeave={handleDropDownLeave}>
               {isDroppedDown &&
@@ -31,12 +43,38 @@ export const Navbar = () => {
               } 
             </div>
           </div>
-          <Link href="/global-community"><p>Global Community</p></Link>
-          <Link href="/shop"><p>Shop</p></Link>
-          <Link href="/the-garden"><p>The Garden</p></Link>
-          <Link href="/connect"><p>Connect</p></Link>
-          <img className="search-icon" src="/images/search2.png"/>
-          <img className="bag-icon" src="/images/shopping-bag2.png"/>
+          <Link href="/global-community">
+            <div className="menu-item">
+              <p>Global Community</p>
+            </div>
+          </Link>
+          <Link href="/shop">
+            <div className="menu-item">
+              <p>Shop</p>
+            </div>
+          </Link>
+          <Link href="/the-garden">
+            <div className="menu-item">
+              <p>The Garden</p>
+            </div>
+          </Link>
+          <Link href="/connect">
+            <div className="menu-item">
+              <p>Connect</p>
+            </div>
+          </Link>
+          <img 
+            onMouseEnter={() => setSearchBlue(true)} 
+            onMouseLeave={() => setSearchBlue(false)} 
+            className="search-icon" 
+            src={isSearchBlue ? "/images/search2-blue.png" : "/images/search2.png"}
+          />
+          <img 
+            onMouseEnter={() => setBagBlue(true)}
+            onMouseLeave={() => setBagBlue(false)}
+            className="bag-icon"
+            src={isBagBlue ? "/images/shopping-bag2-blue.png" : "/images/shopping-bag2.png"}
+          />
         </div>
       </div>
     </div>
